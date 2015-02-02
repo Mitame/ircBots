7711db7c18fe146a92e803b0f6faa8207a2deab0
Emati Mitame <gitcommits@mita.me>
2015-02-02 21:41:21 +0000

    Added time command to display the time in ascii art (requires pyfiglet)

--- Commands/fun.py
+++ Commands/fun.py
@@ -1,5 +1,12 @@
 from mainbot.commands import Command
 
+import datetime
+import time
+import urllib.request
+import urllib.parse
+import json
+import pyfiglet
+
 class slap(Command):
     arguments = ["str"]
     permissionLevel = 3
@@ -11,3 +18,22 @@ class slap(Command):
     def on_call(self, event, *args):
         print(args[0])
         self.bot.connection.privmsg(self.bot.channelName,"\001ACTION slapped %s.\001" % args[0])
+
+class asciiClock(Command):
+    arguments = []
+    permissionLevel = 0
+    permitExtraArgs = False
+    manArgCheck = False
+    defaultArgs = []
+    callname = "time"
+    
+    def on_call(self,event,*args):
+        font = pyfiglet.Figlet()
+        
+        x = font.renderText(str(time.ctime()).split(" ")[4])
+        for line in x.split("\n"):
+            if line != "":
+                self.pubMsg(event,line)
+        
+                                   
+        
