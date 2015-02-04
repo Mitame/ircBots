78e0a421dab80a722b6be10bb1c2188a0a23c0c4
Emati Mitame <gitcommits@mita.me>
2015-02-04 18:02:01 +0000

    Changed configuration use

--- conf.py
+++ conf.py
@@ -1,11 +1,20 @@
+import defaultconf
+
 class settings():
-    host = "irc.rizon.net"
+    host = "home.mrmindimplosion.co.uk"
     port = 6667
-    channel = "#BrokenBots"
+    channel = "#BANANARAMA"
     callsign = "pollbot"
     name = "PollBot"
-    manOplist = ["YourNickHere"]
+    manOplist = ["MrMindImplosion"]
     chatlog = open("./irclogs/chat.log","a")
     allowExclaimCommand = True
     textPrefix = "\x0306,99"
     textPostfix = "\x03\z02"
+    nickPass = "password"
+    version = 1
+
+
+
+if defaultconf.settings.version > settings.version:
+    raise ImportWarning("More settings have been added, please update your conf.py.")
