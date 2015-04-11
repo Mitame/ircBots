356713275985776fcd4aaf776067c83f75cb4b19
Emati Mitame <gitcommits@mita.me>
2015-04-11 17:37:15 +0100

    Overhalled cowsay command so that it now takes the same parameters as the cowsay command on linux.

--- Commands/fun.py
+++ Commands/fun.py
@@ -40,9 +40,30 @@ class cowsay(Command):
     manArgCheck = False
     defaultArgs = []
     callName = "cowsay"
-    
+
+    withArguments = ["-e","-f","-T","-W"]
+    withoutArguments = ["-b", "-d", "-g", "-h", "-l", "-L", "-n", "-N", "-p", "-s", "-t", "-w", "-y"]
+
     def on_call(self,event,*args):
-        y = os.popen("cowsay " + shlex.quote(" ".join(args)))
+        args = list(args)
+        newArgs = []
+        text = []
+        while len(args) != 0:
+            arg = args.pop(0)
+            if arg[0:2] in self.withArguments:
+                newArgs.append(arg[0:2])
+                if arg not in self.withArguments: #check for arguments without space
+                    newArgs.append(arg[2:])
+                else:
+                    newArgs.append(args.pop(0))
+            elif arg in self.withoutArguments:
+                newArgs.append(arg)
+            elif arg[0] == "-":
+                self.privMsg(event,"Invalid argument '%s'." % arg)
+            else:
+                text.append(arg)
+        print("Executing %s." % ("cowsay " + " ".join(newArgs) + " " +shlex.quote(" ".join(text))))
+        y = os.popen("cowsay " + " ".join(newArgs) + " " +shlex.quote(" ".join(text)))
         for line in y.read().split("\n"):
             self.pubMsg(event,line)
 
